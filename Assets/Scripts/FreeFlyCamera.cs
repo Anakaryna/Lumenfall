be529dde8819286e12d67bf3a0cc7b0129d10e9e
Towga <kamiltebn@gmail.com>
2026-04-03 03:18:40 +0200

    add shift control to unlock mouse

--- Assets/Scripts/FreeFlyCamera.cs
+++ Assets/Scripts/FreeFlyCamera.cs
@@ -25,23 +25,59 @@ public class FreeFlyCamera : MonoBehaviour
 
     void Update()
     {
-        HandleMouseLook();
+        HandleCursorToggle();
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+            HandleMouseLook();
+
         HandleMovement();
-        HandleCursorUnlock();
+    }
+
+    void HandleCursorToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     void HandleMouseLook()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseX =
+            Input.GetAxis("Mouse X")
+            * mouseSensitivity
+            * 100f
+            * Time.deltaTime;
+        float mouseY =
+            Input.GetAxis("Mouse Y")
+            * mouseSensitivity
+            * 100f
+            * Time.deltaTime;
 
         rotationX -= mouseY;
         rotationY += mouseX;
 
         rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 
-        float smoothX = Mathf.SmoothDampAngle(transform.eulerAngles.x, rotationX, ref currentVelX, smoothTime);
-        float smoothY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationY, ref currentVelY, smoothTime);
+        float smoothX = Mathf.SmoothDampAngle(
+            transform.eulerAngles.x,
+            rotationX,
+            ref currentVelX,
+            smoothTime
+        );
+        float smoothY = Mathf.SmoothDampAngle(
+            transform.eulerAngles.y,
+            rotationY,
+            ref currentVelY,
+            smoothTime
+        );
 
         transform.rotation = Quaternion.Euler(smoothX, smoothY, 0f);
     }
@@ -50,10 +86,9 @@ public class FreeFlyCamera : MonoBehaviour
     {
         float speed = moveSpeed;
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftControl))
             speed *= sprintMultiplier;
 
-        // ZQSD (AZERTY)
         float forward = 0f;
         float right = 0f;
 
@@ -64,27 +99,9 @@ public class FreeFlyCamera : MonoBehaviour
 
         Vector3 move = transform.forward * forward + transform.right * right;
 
-        // Vertical (A = down, E = up) 
-        if (Input.GetKey(KeyCode.E))
-            move += Vector3.up;
-        if (Input.GetKey(KeyCode.A))
-            move += Vector3.down;
+        if (Input.GetKey(KeyCode.E)) move += Vector3.up;
+        if (Input.GetKey(KeyCode.A)) move += Vector3.down;
 
-        transform.position += move * speed * Time.deltaTime;
-    }
-
-    void HandleCursorUnlock()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+        transform.position += move.normalized * speed * Time.deltaTime;
     }
 }
